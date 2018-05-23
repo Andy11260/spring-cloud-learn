b4dbde0357260849dee4e45760891be7d99771f6
11260 <1126073524@qq.com>
2018-05-23 17:30:00 +0800

    first commit

---
+++ service-simple-provider-user/src/main/java/com/zm/cloud/ServiceSimpleProviderUserApplication.java
@@ -0,0 +1,16 @@
+package com.zm.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * 服务提供者
+ */
+@SpringBootApplication
+public class ServiceSimpleProviderUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceSimpleProviderUserApplication.class, args);
+    }
+}
---
+++ service-simple-provider-user/src/main/java/com/zm/cloud/entity/User.java
@@ -0,0 +1,34 @@
+package com.zm.cloud.entity;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+
+@Data
+@Table(name = "user")
+@Entity
+public class User {
+
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "balance")
+    private BigDecimal balance;
+
+
+}
